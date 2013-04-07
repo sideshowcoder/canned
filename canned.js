ee5a3499982abb029cd0e83198c30ebe1ef4732c
Philipp Fehre <philipp.fehre@googlemail.com>
2013-04-07 22:31:09 +0200

    handle filetype based on extension

--- canned.js
+++ canned.js
@@ -1,12 +1,44 @@
-function Canned(dir) {
+var url = require('url')
+,   fs = require('fs')
+,   util = require('util')
+
+function Canned(dir){
+  this.dir = process.cwd() + '/' + dir
 }
 
-Canned.prototype.responder = function(req, res) {
+Canned.prototype.responder = function(req, res){
+  var pathname = url.parse(req.path).pathname.split('/')
+  ,   dname = pathname.pop()
+  ,   fname = '_' + dname
+  ,   method = req.method.toLowerCase()
+  ,   path = this.dir + pathname.join('/')
+
+  fs.readdir(path, function(err, files){
+    var pattern = fname + '\.' + method + '\.(.+)'
+    ,   ext
+
+    files.forEach(function(el){
+      var m = el.match(new RegExp(pattern))
+
+      if(m) {
+       ext = m[1]
+       return
+      }
+    })
+
+    if(!ext) {
+      res.statusCode = 400
+    } else {
+      res.setHeader('Content-Type', 'application/' + ext)
+      res.statusCode = 200
+    }
+    res.end()
+  })
 }
 
-var canned = function(dir) {
-  can = new Canned(dir)
-  return can.responder
+var canned = function(dir){
+  c = new Canned(dir)
+  return c.responder.bind(c)
 }
 
 module.exports = canned
--- test/canned.test.js
+++ test/canned.test.js
@@ -8,11 +8,60 @@ test('canned actually loads', function(t){
 })
 
 test('create fake api', function(t){
-  t.type(canned('../test_responses'), 'function', 'canned fake api creates success')
+  var can = canned('./test_responses')
+  ,   req = { method: 'GET' }
+  ,   res = { setHeader: function(){}, end: function(){} }
+
+  t.type(can, 'function', 'canned fake api creates success')
+
+  t.test('returns status 400 if _na.get does not exist for /na', function(t){
+    t.plan(1)
+    req.path = '/na'
+    res.end = function(){
+      t.equal(res.statusCode, 400, 'set error status')
+    }
+    can(req, res)
+  })
+
+  t.test('returns status 200 if _a.get.json does exist for /a', function(t){
+    t.plan(1)
+    req.path = '/a'
+    res.end = function(){
+      t.equal(res.statusCode, 200, 'set ok status')
+    }
+    can(req, res)
+  })
+
+  t.test('sets application/json as content-type if _a.get.json does exist for /a', function(t){
+    t.plan(2)
+    req.path = '/a'
+    res.setHeader = function(name, value){
+      t.equal(name, 'Content-Type', 'set content-type')
+      t.equal(value, 'application/json', 'set application/json')
+    }
+    can(req, res)
+  })
+
+  t.test('sets application/txt as content-type if _b.get.txt does exist for /b', function(t){
+    t.plan(1)
+    req.path = '/b'
+    res.setHeader = function(name, value){
+      t.equal(value, 'application/txt', 'set content type txt')
+    }
+    can(req, res)
+  })
+
+  t.test('gets the content of index.get.json for /', { skip: true }, function(t){
+    t.plan(1)
+    req.path = '/'
+    res.end = function(content){
+      t.equal(content, 'index.get.json', 'get the content')
+    }
+    can(req, res)
+  })
+
   t.end()
 })
-
-
 
 
 
