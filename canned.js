c73aecad31b862430658d31498a34d044aa4c678
Philipp Fehre <philipp.fehre@googlemail.com>
2013-07-08 17:46:03 +0200

    support additional options via a custom header
    //! statusCode: 201
    
    //! defines a header with options for the given file which is stripped
    before sending, this is used to provide custom return codes like 201 for
    created in a extensibel manner.
    
    FIX #7
    
    improve on header formate
    
    Issue #7
    
    make the style consistent
    
    fixed comment

--- canned.js
+++ canned.js
@@ -14,13 +14,13 @@ CORS_HEADERS = [
   ['Access-Control-Allow-Headers', 'X-Requested-With'],
 ]
 
-function Canned(dir, options){
+function Canned(dir, options) {
   this.logger = options.logger
   this.cors = options.cors
   this.dir = process.cwd() + '/' + dir
 }
 
-function scanFileListForName(files, pattern){
+function scanFileListForName(files, pattern) {
   var m, i, e
   for (i = 0, e = files[i]; e != null; e = files[++i]) {
     m = e.match(new RegExp(pattern))
@@ -29,13 +29,33 @@ function scanFileListForName(files, pattern){
   return false
 }
 
-function removeJSLikeComments(text){
+function removeJSLikeComments(text) {
   return text.replace(/\/\*.+?\*\/|\/\/.*(?=[\n\r])/g, '')
 }
 
-function sanatize(data, cType){
+Canned.prototype._extractOptions = function(data) {
+  var lines = data.split('\n')
+  var opts = {}
+  if (lines[0].indexOf('//!') !== -1) {
+    try {
+      var content = lines[0].replace('//!','')
+      content = content.split(',').map(function(s) {
+        return '"' + s.split(':')[0].trim() + '":' + s.split(':')[1]
+      }).join(',')
+      opts = JSON.parse('{' + content  + '}')
+    } catch(e) {
+      this._log('Invalid file header format try //! statusCode: 201')
+      opts = {}
+    }
+    lines.splice(0,1)
+  }
+  var statusCode = opts.statusCode || 200
+  return { statusCode: statusCode, data: lines.join('\n') }
+}
+
+function sanatize(data, cType) {
   var sanatized
-  switch(cType) {
+  switch (cType) {
     case 'json':
       // make sure we return valid JSON even so we support comments
       try {
@@ -50,29 +70,32 @@ function sanatize(data, cType){
   return sanatized
 }
 
-Canned.prototype._headers = function(type){
+Canned.prototype._headers = function(type) {
   var headers = [['Content-Type', CONTENT_TYPES[type]]]
-  if(this.cors) headers = headers.concat(CORS_HEADERS)
+  if (this.cors) headers = headers.concat(CORS_HEADERS)
   return headers
 }
 
 // return a data structure representing the response for a file
 // datastructure mirrors Racks [headers, status, content]
 // while headers are [[name, value], [name, value]]
-Canned.prototype._responseForFile = function(fname, path, method, files, cb){
+Canned.prototype._responseForFile = function(fname, path, method, files, cb) {
   var pattern = fname + '\.' + method + '\.(.+)'
   var that = this
   var m
 
-  if(m = scanFileListForName(files, pattern)){
+  if(m = scanFileListForName(files, pattern)) {
     var file = path + '/' + m[0]
-    fs.readFile(file, { encoding: 'utf8' }, function(err, data){
-      if(err) {
+    fs.readFile(file, { encoding: 'utf8' }, function(err, data) {
+      if (err) {
         cb('Not found', [that._headersFor('html'), 404, ''])
       } else {
-        content = sanatize(data, m[1])
-        if(content){
-          cb(null, [that._headers(m[1]), 200, content])
+        var _data = that._extractOptions(data)
+        data = _data.data
+        var statusCode = _data.statusCode
+        var content = sanatize(data, m[1])
+        if (content) {
+          cb(null, [that._headers(m[1]), statusCode, content])
         } else {
           cb(null, [that._headers('html'), 500, 'Internal Server error invalid input file'])
         }
@@ -83,23 +106,23 @@ Canned.prototype._responseForFile = function(fname, path, method, files, cb){
   }
 }
 
-function writeError(res){
+function writeError(res) {
   writeResponse(res, [[['Content-Type', 'text/html']], 500, ''])
 }
 
-function writeResponse(res, resp){
-  for(var i = 0, h = resp[0][i]; h != null; h = resp[0][++i]){
+function writeResponse(res, resp) {
+  for (var i = 0, h = resp[0][i]; h != null; h = resp[0][++i]) {
     res.setHeader(h[0], h[1])
   }
   res.statusCode = resp[1]
   res.end(resp[2])
 }
 
-Canned.prototype._log = function(message){
-  if(this.logger) this.logger.write(message)
+Canned.prototype._log = function(message) {
+  if (this.logger) this.logger.write(message)
 }
 
-Canned.prototype.responder = function(req, res){
+Canned.prototype.responder = function(req, res) {
   var pathname = url.parse(req.url).pathname.split('/')
   ,   dname = pathname.pop()
   ,   fname = '_' + dname
@@ -109,19 +132,19 @@ Canned.prototype.responder = function(req, res){
 
   this._log('request: ' + req.method + ' ' + req.url)
 
-  if(method == 'options' && that.cors) {
+  if (method == 'options' && that.cors) {
     that._log('Options request, serving CORS Headers\n')
     writeResponse(res, [CORS_HEADERS, 200, ''])
     return
   }
 
-  fs.readdir(path, function(err, files){
-    fs.stat(path + '/' + dname, function(err, stats){
-      if(err){
-        that._responseForFile(fname, path, method, files, function(err, resp){
-          if(err) {
-            that._responseForFile('any', path, method, files, function(err, resp){
-              if(err) {
+  fs.readdir(path, function(err, files) {
+    fs.stat(path + '/' + dname, function(err, stats) {
+      if (err) {
+        that._responseForFile(fname, path, method, files, function(err, resp) {
+          if (err) {
+            that._responseForFile('any', path, method, files, function(err, resp) {
+              if (err) {
                 that._log(' not found\n')
               } else {
                 that._log(' served via: ' + pathname.join('/') + '/any.' + method + '\n')
@@ -136,9 +159,9 @@ Canned.prototype.responder = function(req, res){
       } else {
         if(stats.isDirectory()) {
           var fpath = path + '/' + dname
-          fs.readdir(fpath, function(err, files){
-            that._responseForFile('index', fpath, method, files, function(err, resp){
-              if(err) {
+          fs.readdir(fpath, function(err, files) {
+            that._responseForFile('index', fpath, method, files, function(err, resp) {
+              if (err) {
                 that._log(' not found\n')
               } else {
                 that._log(' served via: ' + pathname.concat(dname).join('/') + '/index.' + method + '\n')
@@ -154,8 +177,8 @@ Canned.prototype.responder = function(req, res){
   })
 }
 
-var canned = function(dir, options){
-  if(!options) options = {}
+var canned = function(dir, options) {
+  if (!options) options = {}
   c = new Canned(dir, options)
   return c.responder.bind(c)
 }
--- test/canned.test.js
+++ test/canned.test.js
@@ -3,120 +3,129 @@ var tap = require('tap')
 ,   canned = require('../canned')
 ,   fs = require('fs')
 
-test('canned actually loads', function(t){
+test('canned actually loads', function(t) {
   t.ok(canned, 'canned loaded successfully')
   t.end()
 })
 
-test('create fake api', function(t){
+test('create fake api', function(t) {
   var can = canned('./test_responses', { logger: process.stderr })
   ,   req = { method: 'GET' }
-  ,   res = { setHeader: function(){}, end: function(){} }
+  ,   res = { setHeader: function() {}, end: function() {} }
 
 
   t.type(can, 'function', 'canned fake api creates success')
 
-  t.test('returns status 404 if _na.get does not exist for /na', function(t){
+  t.test('returns status 404 if _na.get does not exist for /na', function(t) {
     t.plan(1)
     req.url = '/na'
-    res.end = function(){
+    res.end = function() {
       t.equal(res.statusCode, 404, 'set error status')
     }
     can(req, res)
   })
 
-  t.test('returns status 200 if _a.get.json does exist for /a', function(t){
+  t.test('returns status 201 if set in file', function(t) {
+    t.plan(1)
+    req.url = '/201'
+    res.end = function() {
+      t.equal(res.statusCode, 201, 'set status code from file')
+    }
+    can(req, res)
+  })
+
+  t.test('returns status 200 if _a.get.json does exist for /a', function(t) {
     t.plan(1)
     req.url = '/a'
-    res.end = function(){
+    res.end = function() {
       t.equal(res.statusCode, 200, 'set ok status')
     }
     can(req, res)
   })
 
-  t.test('sets application/txt as content-type if _b.get.txt does exist for /b', function(t){
+  t.test('sets application/txt as content-type if _b.get.txt does exist for /b', function(t) {
     t.plan(1)
     req.url = '/b'
     var expected_headers = { 'Content-Type': "text/plain" }
-    res.setHeader = function(name, value){
+    res.setHeader = function(name, value) {
       t.equal(value, expected_headers[name], 'Checking Header '+name)
     }
     can(req, res)
   })
 
-  t.test('gets content-type json if index.get.html for /', function(t){
+  t.test('gets content-type json if index.get.html for /', function(t) {
     t.plan(1)
     req.url = '/'
     var expected_headers = { 'Content-Type': "text/html" }
-    res.setHeader = function(name, value){
+    res.setHeader = function(name, value) {
       t.equal(value, expected_headers[name], 'Checking Header '+name)
     }
     can(req, res)
   })
 
-  t.test('gets content of index.get.json for /', function(t){
+  t.test('gets content of index.get.json for /', function(t) {
     t.plan(1)
     req.url = '/'
-    res.end = function(content){
+    res.end = function(content) {
       t.equal(content, 'index.get.json\n', 'set the file content')
     }
     can(req, res)
   })
 
-  t.test('gets _a.get.json for /a?abc=thegreatpubar ignoring the query params', function(t){
+  t.test('gets _a.get.json for /a?abc=thegreatpubar ignoring the query params', function(t) {
     t.plan(1)
     req.url = '/a?abc=thegreatpubar'
-    res.end = function(content){
+    res.end = function(content) {
       t.equal(content, '_a.get.json\n', 'set the file content')
     }
     can(req, res)
   })
 
-  t.test('gets content of index.get.json for /d', function(t){
+  t.test('gets content of index.get.json for /d', function(t) {
     t.plan(1)
     req.url = '/d'
-    res.end = function(content){
+    res.end = function(content) {
       t.equal(content, 'd/index.get.json\n', 'set the file content')
     }
     can(req, res)
   })
 
-  t.test('gets content of any.get.json for /d/iamanid', function(t){
+  t.test('gets content of any.get.json for /d/iamanid', function(t) {
     t.plan(1)
     req.url = '/d/iamanid'
-    res.end = function(content){
+    res.end = function(content) {
       t.equal(content, 'd/any.get.json\n', 'set the file content')
     }
     can(req, res)
   })
 
-  t.test('removes the comments from .json so it is valid', function(t){
+  t.test('removes the comments from .json so it is valid', function(t) {
     t.plan(1)
     req.url = '/d/commented'
-    res.end = function(content){
+    res.end = function(content) {
       t.equal(content, '{"no":"comments"}', 'removes comments')
     }
     can(req, res)
   })
-  t.test('CORS support', function(t){
+  t.test('CORS support', function(t) {
     var can = canned('./test_responses', { logger: process.stderr, cors: true })
     ,   req = { method: 'GET' }
-    ,   res = { setHeader: function(){}, end: function(){} }
+    ,   res = { setHeader: function() {}, end: function() {} }
 
-    t.test('sets the necessary HEADER to allow CORS', function(t){
+    t.test('sets the necessary HEADER to allow CORS', function(t) {
       t.plan(2)
       var req = { method: 'OPTIONS', url: '/' }
       var expected_headers = {
         'Access-Control-Allow-Origin': "*"
       , 'Access-Control-Allow-Headers': "X-Requested-With"
       }
-      res.setHeader = function(name, value){
+      res.setHeader = function(name, value) {
         t.equal(value, expected_headers[name], 'Checking Header '+name)
       }
       can(req, res)
     })
 
-    t.test('sets the necessary HEADER to allow CORS on GET', function(t){
+    t.test('sets the necessary HEADER to allow CORS on GET', function(t) {
       t.plan(3)
       req.url = '/'
       var expected_headers = {
@@ -124,7 +133,7 @@ test('create fake api', function(t){
       , 'Access-Control-Allow-Origin': "*"
       , 'Access-Control-Allow-Headers': "X-Requested-With"
       }
-      res.setHeader = function(name, value){
+      res.setHeader = function(name, value) {
         t.equal(value, expected_headers[name], 'Checking Header '+name)
       }
       can(req, res)
