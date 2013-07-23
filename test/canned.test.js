412c73cb84ac78bf15581efcf55ec0696f290eb1
Philipp Fehre <philipp.fehre@googlemail.com>
2013-07-23 09:48:48 +0200

    move all the tests!!!

--- test/canned.test.js
+++ test/canned.test.js
@@ -16,56 +16,11 @@ test('create fake api', function(t) {
 
   t.type(can, 'function', 'canned fake api creates success')
 
-  t.test('gets content of index.get.json for /', function(t) {
-    t.plan(1)
-    req.url = '/'
-    res.end = function(content) {
-      t.equal(content, 'index.get.json\n', 'set the file content')
-    }
-    can(req, res)
-  })
-
-  t.test('gets _a.get.json for /a?abc=thegreatpubar ignoring the query params', function(t) {
-    t.plan(1)
-    req.url = '/a?abc=thegreatpubar'
-    res.end = function(content) {
-      t.equal(content, '_a.get.json\n', 'set the file content')
-    }
-    can(req, res)
-  })
-
-  t.test('gets content of index.get.json for /d', function(t) {
-    t.plan(1)
-    req.url = '/d'
-    res.end = function(content) {
-      t.equal(content, 'd/index.get.json\n', 'set the file content')
-    }
-    can(req, res)
-  })
-
-  t.test('gets content of any.get.json for /d/iamanid', function(t) {
-    t.plan(1)
-    req.url = '/d/iamanid'
-    res.end = function(content) {
-      t.equal(content, 'd/any.get.json\n', 'set the file content')
-    }
-    can(req, res)
-  })
-
   t.test('removes the comments from .json so it is valid', function(t) {
     t.plan(1)
     req.url = '/d/commented'
     res.end = function(content) {
       t.equal(content, '{"no":"comments"}', 'removes comments')
-    }
-    can(req, res)
-  })
-
-  t.test('nested folder request for not present folders should work', function(t) {
-    t.plan(1)
-    req.url = '/foo/bar/baz'
-    res.end = function() {
-      t.equal(res.statusCode, 404, 'set not status')
     }
     can(req, res)
   })
