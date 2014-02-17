6f69b7affa9d69d5e24d64576f2cbc450b7caba1
Camille Reynders <camille.reynders@gmail.com>
2014-02-17 09:37:58 +0100

    fixes incorrect default opts and cli startup

--- canned.js
+++ canned.js
@@ -3,6 +3,7 @@
 var _ = require('lodash');
 var express = require('express');
 var Args = require('args-js');
+var defaultPort = 3000;
 
 function serve(dir){
 	return require('./lib/parser')(dir);
@@ -10,16 +11,19 @@ function serve(dir){
 
 function Canned(dir, opts){
 	this._dir = dir;
-	this._opts = _.defaults({
-		prefix : ''
-	}, opts);
+	this._opts = _.defaults(opts, {
+		prefix : '',
+		port : defaultPort
+	});
+	console.log(this._opts);
 };
 
 Canned.prototype.listen = function(port, app){
 	var args = Args([
-		{port : Args.INT | Args.Optional, _default : 3000},
+		{port : Args.INT | Args.Optional, _default : this._opts.port},
 		{app : Args.OBJECT | Args.Optional}
 	], arguments);
+	this._opts.port = args.port;
 	var server = this._server = args.app || express();
 	server.use(express.logger());
 	server.use(this._opts.prefix, serve(this._dir));
@@ -33,4 +37,5 @@ function createCanned(dir, opts){
 };
 
 createCanned.serve = serve;
+createCanned.defaultPort = defaultPort;
 module.exports = createCanned;
