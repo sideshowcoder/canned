8fa7257ef1622624866b5bbe90c874200b78a9b2
Camille Reynders <camille.reynders@gmail.com>
2014-02-17 09:59:56 +0100

    reformatted source

--- canned.js
+++ canned.js
@@ -6,34 +6,34 @@ var Args = require('args-js');
 var defaultPort = 3000;
 
 function serve(dir){
-	return require('./lib/parser')(dir);
+  return require('./lib/parser')(dir);
 };
 
 function Canned(dir, opts){
-	this._dir = dir;
-	this._opts = _.defaults(opts, {
-		prefix : '',
-		port : defaultPort
-	});
-	console.log(this._opts);
+  this._dir = dir;
+  this._opts = _.defaults(opts, {
+    prefix : '',
+    port   : defaultPort
+  });
+  console.log(this._opts);
 };
 
 Canned.prototype.listen = function(port, app){
-	var args = Args([
-		{port : Args.INT | Args.Optional, _default : this._opts.port},
-		{app : Args.OBJECT | Args.Optional}
-	], arguments);
-	this._opts.port = args.port;
-	var server = this._server = args.app || express();
-	server.use(express.logger());
-	server.use(this._opts.prefix, serve(this._dir));
-	server.listen(args.port);
+  var args = Args([
+    {port : Args.INT | Args.Optional, _default : this._opts.port},
+    {app : Args.OBJECT | Args.Optional}
+  ], arguments);
+  this._opts.port = args.port;
+  var server = this._server = args.app || express();
+  server.use(express.logger());
+  server.use(this._opts.prefix, serve(this._dir));
+  server.listen(args.port);
 };
 
 function createCanned(dir, opts){
-	var canned = new Canned(dir, opts);
-	canned.listen();
-	return canned;
+  var canned = new Canned(dir, opts);
+  canned.listen();
+  return canned;
 };
 
 createCanned.serve = serve;
