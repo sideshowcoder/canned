e96e1b11d0e69cec68808f04b8cd6b6fa5d98c89
Matias Emanuel Surdi <msurdi@qdqmedia.com>
2015-06-07 18:49:33 +0200

    Add unit test for lookup function

---
+++ spec/lookup.spec.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var lookup = require('../lib/lookup');
+
+describe('lookup', function () {
+  it('should generate a list of paths in the correct order', function (done) {
+    var testPath = '/api/2/customer/123/invoice/321/';
+    var expectedPaths = [
+      '/api/2/customer/123/invoice/321/',
+      '/api/2/customer/123/invoice/any/',
+      '/api/2/customer/any/invoice/321/',
+      '/api/2/customer/any/invoice/any/',
+      '/api/any/customer/123/invoice/321/',
+      '/api/any/customer/123/invoice/any/',
+      '/api/any/customer/any/invoice/321/',
+      '/api/any/customer/any/invoice/any/'
+    ];
+    var generatedPaths = lookup.getPaths(testPath, 'any');
+    expect(generatedPaths).toEqual(expectedPaths);
+    done();
+  });
+});
